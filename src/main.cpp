bd774c7ed795657326a634d76859476e2962d95b
Nivedha Karthikeyan <nivy77@gmail.com>
2018-11-14 14:35:44 -0800

    Started main.cpp basic framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main () {
+int main (int argc, char** argv) {
+    // need to implement 3 componenets
+    // 1) Initialize shell with $
+    // 2) Interpret and execute commands
+    // 3) How/when to terminate
+   
+    // Start a main command loop
+    rshell_loop();
 
+
+    // Determine when to exit 
+   
     return 0;
 }
