44085d1b247be8483bc46d6954dda7d458c85c9d
Nivedha Karthikeyan <nivy77@gmail.com>
2018-11-15 11:40:58 -0800

    Test.cpp Framework updated

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,39 @@
 #include "gtest/gtest.h"
 #include <iostream>
 
-TEST(EchoTest, factory_list){
+
+//The assignment specs requires us to test four overall cases:
+// 1) Executing single commands
+// 2) Executing commands with connectors
+// 3) Executing commands with comments 
+// 4) Exit commands
+//
+//
+TEST(EchoTest, SingleCommands){
     //EXPECT_EQ(, );
+    
+
 }
+
+
+TEST(EchoTest, ConnectorCommands){
+    //EXPECT_EQ();
+
+}    
+
+
+TEST(EchoTest, CommentCommands){
+    //EXPECT_EQ();
+
+}
+
+
+TEST(EchoTest, ExitCommands){
+    //EXPECT_EQ();
+
+}
+
+
 
 
 int main(int argc, char **argv) {
