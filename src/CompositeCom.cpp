692029a8d870c96ac7c3196f262982f976ed958b
Erin Wong <erin.b.wong@gmail.com>
2018-11-14 19:30:12 -0800

    Initialize executables for headers
    Copy-pasted directly over from /headers
    Only changed the file path/name

---
+++ src/CompositeCom.cpp
@@ -0,0 +1,19 @@
+#ifndef COMPOSITECOM_H
+#define COMPOSITECOM_H
+
+#include "Commands.h"
+
+class CompositeCom : public Commands {
+    protected:
+        Commands* left;
+        Commands* right;
+
+    public:
+        CompositeCom() {}
+        CompositeCom(std::string command_in, Commands* l, Commands* r) : Commands(command_in), left(l), right(r) {}
+        
+        Commands* parse();
+        int execute();
+};
+
+#endif // COMPOSITECOM_H
