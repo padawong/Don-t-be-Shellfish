4b3224e4f2a637d8f0c65912e3410ad9bca78bee
Nivedha Karthikeyan <nivy77@gmail.com>
2018-11-14 17:49:30 -0800

    Update #1 on basic framework for execute file

--- src/exec_loop.h
+++ src/exec_loop.h
@@ -2,5 +2,23 @@
 #include <iostream>
 
 rshell_loop(){
+    char* line;
+    char** args;
+    int cont;
+
+    
+    do{
+        cout << "$ ";
+        line = rshell_read();
+        args = rshell_split();
+        status = rshell_execute(args);
+
+        //idk how to clear these variables after each iteration since 
+        //they are pointers
+        line = null;
+        args = null;
+        
+    } while (cont);    
+
 
 }
