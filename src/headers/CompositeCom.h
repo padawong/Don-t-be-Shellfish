370b2c3bc93dcc700956c4a1fa260f4d28759dac
Nivedha Karthikeyan <nivy77@gmail.com>
2018-11-14 21:23:45 -0800

    Changed parse and execute to return bool

--- src/headers/CompositeCom.h
+++ src/headers/CompositeCom.h
@@ -13,8 +13,8 @@ class CompositeCom : public Commands {
         CompositeCom(std::string command_in);
         // CompositeCom(std::string command_in, Commands* l, Commands* r) : Commands(command_in), left(l), right(r) {}
         
-        Commands* parse();
-        int execute();
+        bool parse();
+        bool execute();
 };
 
 #endif // COMPOSITECOM_H
