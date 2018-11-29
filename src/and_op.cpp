feba70c1dd7734e88e2a870d7dbfb86237516b26
Erin Wong <erin.b.wong@gmail.com>
2018-11-28 17:58:00 -0800

    Create/initialize Binary::And

---
+++ src/and_op.cpp
@@ -0,0 +1,9 @@
+#include "and_op.h"
+
+And_Op::And_Op() {}
+
+And_Op::And_Op(Commands* l, Commands* r) : BinaryComp(l, r) {}
+
+bool And_Op::execute() {
+
+}
---
+++ src/headers/and_op.h
@@ -0,0 +1,14 @@
+#ifndef AND_OP_H
+#define AND_OP_H
+
+#include "BinaryComp.h"
+
+class And_Op : public BinaryComp {
+    public:
+        And_Op();
+        And_Op(Commands* l, Commands* r);
+
+        bool execute();
+}
+
+#endif // AND_OP_H
