8c1c5db8b96d461f979acc16ba14fee68ff47749
Erin Wong <erin.b.wong@gmail.com>
2018-11-30 21:51:59 -0800

    Major revisions; add verifications, tidy code
    Tests are still not working right. Fixing next.

--- src/TestCom.cpp
+++ src/TestCom.cpp
@@ -1,64 +1,81 @@
 #include "./headers/TestCom.h"
 
-        TestCom::TestCom() : SingleCom() {}
-        TestCom::TestCom(std::vector<std::string>& cmd_v) : SingleCom(cmd_v) {}
+TestCom::TestCom() : SingleCom() {}
+TestCom::TestCom(std::vector<std::string>& cmd_v) : SingleCom(cmd_v) {}
             
-        
-        bool TestCom::testLine(){//RETURN A STRING OR A BOOL????
-          
-            // Array of char* w/ 1 more element than vector to end w/ NULL
-            char* args[commands_vect.size()];
-            
-            // First element is always the command
-            args[0] = (char*)commands_vect.at(0).c_str();
-            
-            // All subsequent elements are arguments
-            for (int i = 1; i < commands_vect.size(); i++) {
-                args[i] = (char*)commands_vect.at(i).c_str(); 
-            }
-
-
-
-            struct stat sb;
-            std::string flag = args[0];
-  
-            //CASE (-f): Is it a regular file or not?            
-            if( flag == "-f"){
-               if (S_ISREG(sb.st_mode) != 0){
-                    std::cout<< "(True)" << std::endl;
-                    return true;   
-               }    
-                else{
-                   std::cout<< "(False)" << std::endl;
-                    return false;
-                }
-            }
-
-            //CASE (-d): Is it a valid directory or not?
-            else if (flag == "-d"){
-                if(S_ISDIR(sb.st_mode) != 0){
-                    std::cout << "(True)" << std::endl;
-                    return true;
-                }
-                else{
-                    std::cout << "(False)" << std::endl;
-                    return false;
-                }
-            }
-
-            //CASE: Is it a vaild filepath or not?
-            else{
-                if(stat(args[1], &sb)){
-                    std::cout << "(True)" << std::endl;
-                    return true;
-                }
-                else{
-                    std::cout << "(False)" << std::endl;
-                    return false;
-                }
-            }
-
+bool TestCom::testLine() {
+    // TEST REMOVE
+   /* std::cout << "commands_vect = ";
+    for (int i = 0; i < commands_vect.size(); i++) {
+        for (int j = 0; j < commands_vect.at(i).size(); j++) {
+            std::cout << commands_vect.at(i).at(j);
         }
+        std::cout << std::endl;
+    }*/
+ 
+    // Default value of flag is -e
+    std::string flag = "-e";
+    
+    // If there is a flag, set it and remove it from the vector
+    if (commands_vect.at(0).at(0) == '-') {
+        flag = commands_vect.at(0);
+        commands_vect.erase(commands_vect.begin());
+    }
+        
+    // If there is not just one string at this point, input is invalid. Return false
+    if (commands_vect.size() != 1) {
+        std::cout << "Invalid path input" << std::endl;
+        return false;
+    }
+
+    // Convert vector of strings into char* array
+    char* args = (char*)commands_vect.at(0).c_str();
+            
+    struct stat sb;
+
+    //CASE: Is it a vaild filepath or not?
+    if (flag == "-e") {
+        if(stat(args, &sb)){
+            std::cout << "(True)" << std::endl;
+            return true;
+        }
+        else{
+            std::cout << "(False)" << std::endl;
+            return false;
+        }
+    }
+    
+    //CASE (-f): Is it a regular file or not?            
+    else if (flag == "-f") {
+       if (S_ISREG(sb.st_mode) != 0){
+            std::cout<< "(True)" << std::endl;
+            return true;   
+       }    
+        else{
+           std::cout<< "(False)" << std::endl;
+            return false;
+        }
+    }
+
+    //CASE (-d): Is it a valid directory or not?
+    else if (flag == "-d") {
+        if(S_ISDIR(sb.st_mode) != 0){
+            std::cout << "(True)" << std::endl;
+            return true;
+        }
+        else{
+            std::cout << "(False)" << std::endl;
+            return false;
+        }
+    }
+
+    // First char in the string was '-' but doesn't match a valid flag
+    else{
+        std::cout << "Invalid flag" << std::endl;
+        return false;
+    }
+
+}
 
 
 
